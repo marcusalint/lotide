1954c4caf47b42474712dd1a8c7bd82cb0fe6d90
salar taeb <salartaeb10@gmail.com>
2020-10-29 02:41:45 +0000

    returns object with keys representing letters, and the count of their values in a string

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`)
+  }
+
+};
+
+const countLetters = function(string) {
+  let result = {};
+  for (letter of string) {
+    if (letter !== ' ') {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(countLetters('Lighthouse Labs'));
+
