5795152c6a6b372cdb98fbe4983d15c2628e3027
salar taeb <salartaeb10@gmail.com>
2020-11-03 10:14:18 +0000

    refactored

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`)
-  }
-
-};
+const assertEqual = require('./assertEqual')
 
 
 const head = function (array) {
@@ -13,8 +6,9 @@ const head = function (array) {
   
 }
 
+module.exports = head;
 
 
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+// console.log(assertEqual(head([5,6,7]), 5));
+// console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
