bca2d037a8a99544265f90aa5774ec9c9f2bb3aa
salar taeb <salartaeb10@gmail.com>
2020-11-03 21:22:11 +0000

    refactored

--- tail.js
+++ tail.js
@@ -13,10 +13,5 @@ const tail = function(array) {
   return array.slice(1);
 };
 
+module.exports = tail
 
-console.log(tail([1,2,3,4,5]));
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
