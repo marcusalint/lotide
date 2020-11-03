eba2e37ee649a2258dfba9d56ddb26b15d676632
salar taeb <salartaeb10@gmail.com>
2020-11-03 22:01:53 +0000

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,8 @@
+
+const tail = require("./eqArrays");
+
+
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected) === true) {
     console.log(`✅ Assertion Passed : ${actual} === ${expected}`);
@@ -6,14 +11,7 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1 [i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const tail = require("../tail");
+
+
+module.exports = assertArraysEqual
