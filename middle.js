4f7ca9fd57dbcf2315ea9077c9f568c527095e28
salar taeb <salartaeb10@gmail.com>
2020-11-03 22:05:55 +0000

    refactored

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-// const eqArrays = function(array1, array2) {
-//   if (array1.length !== array2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1 [i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = function(actual, expected) {
-//   if (eqArrays(actual, expected) === true) {
-//     console.log(`✅ Assertion Passed : ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑 Assertion Failed : ${actual} !== ${expected}`);
-//   }
-// };
-
 
 const middle = function(array) {
   let output = [];
@@ -33,6 +13,4 @@ const middle = function(array) {
 }
 
 
-console.log(middle([1,2,3,4,5]))
-console.log(middle([1,2,3,4,5,6]))
-console.log(middle([1,2]))
+module.exports = middle
