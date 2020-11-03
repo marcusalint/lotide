70c5011a94305925f763c2b8c53628b186467187
salar taeb <salartaeb10@gmail.com>
2020-11-03 21:51:16 +0000

    refactored

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,15 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+const { assert } = require("chai");
+const tail = require("../tail");
 
-
-
-
-
-
-
-
-console.log(tail([1,2,3,4,5]));
-
-
-const words = ["Mini Mo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+  it("returns 2 for tail(words).length", () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+  it("returns ['Lighthouse', 'Labs'] for tail(words)", () => {
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+});
